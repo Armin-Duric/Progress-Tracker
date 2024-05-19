26082265425e9838f2bcc6042b46cc1bf2a65f88
unknown <the_caesar_001@hotmail.com>
2024-05-19 20:10:51 +0200

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,20 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="border-2 grid grid-cols-8 grid-rows-3 w-full h-full">
-      <div className="flex w-full border-2 col-span-2 row-span-3">
+    <main className="border-2 grid grid-cols-6 grid-rows-9 w-full h-full">
+      <div className="flex w-full border-2 col-span-1 row-span-9">
         Profile section
       </div>
-      <nav className="flex w-full border-2 col-span-6">Navbar</nav>
-      <div className="w-full h-full flex border-2 col-span-4">
+      <nav className="flex w-full h-full border-2 col-span-5 row-span-1">
+        Navbar
+      </nav>
+      <div className="w-full h-full flex border-2 col-span-4 row-span-6">
         Greetings Section
       </div>
-      <div className="flex w-full border-2 col-span-2">Learning Section</div>
-      <footer className="flex w-full h-[50px] border-2 col-span-6">
+      <div className="flex w-full h-full border-2 col-span-1 row-span-6">
+        Learning Section
+      </div>
+      <footer className="flex w-full h-full border-2 col-span-5 row-span-2">
         Footer
       </footer>
     </main>
