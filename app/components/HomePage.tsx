dc175d2a8dc969f77aec98b287a0b8555b12e199
unknown <the_caesar_001@hotmail.com>
2024-05-20 10:15:59 +0200

    Initial commit

--- app/components/HomePage.tsx
+++ app/components/HomePage.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+"use client";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Image from "next/image";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const URL = "https://zenquotes.io/api/image";
+  const [data, setData] = useState({});
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(URL);
+      setData(response);
+      console.log(data);
+    } catch (error) {
+      console.error(`Error fetching data: ${error}`);
+    }
+  };
+
+  useEffect(() => {
+    fetchData;
+  });
+
+  return (
+    <div>
+      <img
+        src="https://zenquotes.io/api/image"
+        alt="Image"
+        height={100}
+        width={100}
+      />
+    </div>
+  );
 };
 
 export default HomePage;
