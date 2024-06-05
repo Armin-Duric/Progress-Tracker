31247dc68987efab7cd47a9b3abade7c47316084
unknown <the_caesar_001@hotmail.com>
2024-06-06 00:01:30 +0200

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,8 +16,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="w-full h-full">
+      <body className={inter.className}>
+        <div className="">Profile section</div>
+        <nav className="">
+          <Navbar />
+        </nav>
+        <div className="">{children}</div>
+
+        <div className="">Learning Section</div>
+        <footer className="">
+          <Footer />
+        </footer>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,11 @@
-import Link from "next/link";
-import Navbar from "./components/Navbar";
 import HomePage from "./components/HomePage";
-import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    <main className="border-2 grid grid-cols-6 grid-rows-9 w-full h-full">
-      <div className="flex w-full border-2 col-span-1 row-span-9">
-        Profile section
-      </div>
-      <nav className="flex w-full h-full border-2 col-span-5 row-span-1">
-        <Navbar />
-      </nav>
-      <div className="w-full h-full flex border-2 col-span-4 row-span-7">
+    <main>
+      <div className="">
         <HomePage />
       </div>
-      <div className="flex w-full h-full border-2 col-span-1 row-span-8">
-        Learning Section
-      </div>
-      <footer className="flex w-full h-full border-2 col-span-4 row-span-1">
-        <Footer />
-      </footer>
     </main>
   );
 }
